45a62d93285914737e07242cc7d6bc70d1183e75
Sébastien Bouvier <sebastien@kaliz.fr>
2022-04-13 10:11:00 +0700

    complete triangle project enlightenment

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,20 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # all sides are the same length
+  equal_sides = [
+    a == c,
+    a == b,
+    b == c,
+  ]
+  count = equal_sides.inject(0){ |sum, is_equal| is_equal ? sum+1 : sum }
+  if count == 3
+    :equilateral
+  elsif count == 1
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
